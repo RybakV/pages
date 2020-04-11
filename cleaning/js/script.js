5a80cf8d6262a4aa0bf323b9caed1ddc4b7f650c
Viacheslav Rybak <viacheslav.rybak.pb@gmail.com>
2020-04-11 07:09:58 +0300

    Priority Titans update
    updated logo, brand colors, header, hero section and reviews section

--- cleaning/js/script.js
+++ cleaning/js/script.js
@@ -28,3 +28,36 @@ for (let anchor of anchors) {
 	});
 }
 // END Anchor scrolls
+
+
+// SLIDER reviews
+const previousBtn = document.querySelector('.slider__previous')
+const nextBtn = document.querySelector('.slider__next')
+
+const slides = document.querySelector('.slider__slides')
+const slidesNumber = slides.children.length
+
+// Numbered from 0
+let currentSlide = 0
+
+const updateSlider = () => {
+  const slideWidth = slides.clientWidth
+  const offset = -currentSlide * slideWidth
+
+  slides.setAttribute('style', 'transform: translate(' + offset + 'px)')
+}
+
+previousBtn.addEventListener("click", () => {
+  --currentSlide
+  if (currentSlide < 0) currentSlide = slidesNumber - 1
+
+  updateSlider()
+})
+
+nextBtn.addEventListener('click', () => {
+  ++currentSlide
+  if (currentSlide >= slidesNumber) currentSlide = 0
+
+  updateSlider()
+})
+// END SLIDER
